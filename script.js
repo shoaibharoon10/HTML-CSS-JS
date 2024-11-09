e4bd5c017efdf858695bfdf91573bd745f248415
Muhammmad Shoaib <165128079+shoaibharoon10@users.noreply.github.com>
2024-11-09 19:25:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to toggle the visibility of the navigation menu on smaller screens
+function toggleMenu() {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("show");
+}
